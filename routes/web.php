05860e65121e685da1639f0ece0b290f7298f2c5
rafaely <rafaelydaf@hotmail.com>
2025-10-04 22:15:00 -0600

    Fix vistas

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductosController;
-use App\Http\Controllers\CategoriaController;
-use App\Http\Controllers\HomeController;
 
-// Ruta principal: carga la vista 'home' con datos desde el controlador
-Route::get('/', [HomeController::class, 'index'])->name('home');
+use Yajra\DataTables\Facades\DataTables;
 
-// Vista directa de productos
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
 Route::get('/productos', function () {
     return view('productos');
 });
 
-// Consulta dinámica de productos
+//Ruta para consulta productos
 Route::post('/productos', [ProductosController::class, 'consultaProductos'])->name('productos.consulta');
 
-// Ruta para mostrar una categoría específica
-Route::get('/categorias/{slug}', [CategoriaController::class, 'show'])->name('categorias.show');
